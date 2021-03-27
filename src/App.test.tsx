5d2c89e3d41fee2b7fee6102f6a58b36ee3681d1
hellomuthu23 <hellomuthu23@gmail.com>
2021-03-27 20:57:38 +0000

    test: fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(
+    /React useReducer and useContext example Poker App/i
+  );
   expect(linkElement).toBeInTheDocument();
 });
